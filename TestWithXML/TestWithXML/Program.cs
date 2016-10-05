1a0f3b1d42d134ddaccfea940da7cd49411d232f
NinjaGiskard <NinjaGiskard@KING_PC>
2016-10-05 11:06:09 -0600

    created xml testing

---
+++ TestWithXML/TestWithXML/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestWithXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (XmlWriter writer = XmlWriter.Create("testXmlFile.xml"))
+            {
+
+                writer.WriteStartDocument();
+
+                writer.WriteStartElement("book");
+
+                
+                writer.WriteAttributeString("name", "the Foundation");
+
+                
+
+                writer.WriteElementString("price", "19.95");
+
+                // Write the close tag for the root element.
+                writer.WriteEndElement();
+
+                writer.WriteEndDocument();
+            }
+
+            using (XmlReader reader = XmlReader.Create("testXmlFile.xml"))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        if (reader.HasAttributes)
+                        {
+                            Console.WriteLine("start element with Attributes: " + reader.Name);
+                            Console.WriteLine("\t attribute: " + reader.GetAttribute(0));
+
+                        }
+                        else
+                        {
+                            if(!reader.IsEmptyElement)
+                            {
+                                Console.WriteLine("start element without Attributes: " + reader.Name);
+
+                                if(reader.Name.Equals("price"))
+                                {
+                                    reader.Read();
+                                    Console.WriteLine("\t value: " + reader.Value);
+                                }
+                            }
+
+
+                        }
+                    }
+
+                    
+                }
+            }
+
+            Console.WriteLine("done");
+                Console.Read();
+
+        }
+    }
+}
--- Test_VisualStudios/ConsoleApplication/Program.cs
+++ Test_VisualStudios/ConsoleApplication/Program.cs
@@ -10,8 +10,60 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Program test = new Program();
+            int[] data = { 20, 41, 38, 51, 98, 23, 13, 92, 91, 25, 17, 14, 40, 79, 51, 10, 15, 53, 64, 81 };
+            test.sort(data, 0, 19);
             Console.Read();
         }
+
+        void sort(int[] data, int lowIndex, int highIndex)
+        {
+            Console.Write("Entering sort: ");
+            Console.Write(lowIndex);
+            Console.Write(" ");
+            Console.Write(highIndex);
+            Console.Write("\n");
+
+            if (lowIndex >= highIndex)
+            {
+                Console.Write("Exiting sort: ");
+                Console.Write(lowIndex);
+                Console.Write(" ");
+                Console.Write(highIndex);
+                Console.Write("\n");
+                return;
+            }
+
+            int centerValue = data[highIndex];
+            int centerIndex = lowIndex;
+            int tempIndex = lowIndex;
+            int tempValue;
+
+            while (tempIndex < highIndex)
+            {
+                tempValue = data[tempIndex];
+                if (tempValue < centerValue)
+                {
+                    data[tempIndex] = data[centerIndex];
+                    data[centerIndex] = tempValue;
+                    centerIndex = centerIndex + 1;
+                }
+                tempIndex = tempIndex + 1;
+            }
+
+            data[highIndex] = data[centerIndex];
+            data[centerIndex] = centerValue;
+
+            sort(data, lowIndex, centerIndex - 1);
+
+            sort(data, centerIndex + 1, highIndex);
+
+            Console.Write("Exiting sort: ");
+            Console.Write(lowIndex);
+            Console.Write(" ");
+            Console.Write(highIndex);
+            Console.Write("\n");
+            return;
+}
     }
 }
